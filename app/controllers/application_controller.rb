43516cb109bff90bef0dc62bb10249852690e6d9
Stephen Wright <scw248@gmail.com>
2019-02-28 23:40:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     # @user = User.new(:username => params[:username], :password => params[:password])
     
-    if params[:username] && params[:password]
+    if params[:username] == "" || params[:password] == ""
       binding.pry
       redirect '/failure'
     else
